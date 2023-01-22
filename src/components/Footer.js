001c273acd28ed1a75d981d5e314596f66ee4ccc
robsquaadd <102495137+robsquaadd@users.noreply.github.com>
2023-01-22 16:00:41 -0500

    added linkedin logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import instagram from "../assets/images/instagram.jpg";
 import github from "../assets/images/github.png";
+import linkedin from "../assets/images/linkedin-logo.png";
 
 function Footer() {
   return (
@@ -16,6 +17,14 @@ function Footer() {
         </a>
         <a href="https://www.github.com/robsquaadd/">
           <img src={github} width="30px" height="30px" alt="github logo"></img>
+        </a>
+        <a href="https://www.linkedin.com/in/robert-collier-95058816b/">
+          <img
+            src={linkedin}
+            width="30px"
+            height="30px"
+            alt="linkedin-logo"
+          ></img>
         </a>
       </div>
     </footer>
